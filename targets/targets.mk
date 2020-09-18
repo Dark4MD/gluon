678ca532e94e176703c10ed9a045a1d81faefd72
David Bauer <mail@david-bauer.net>
2020-09-18 16:19:50 +0200

    ar71xx: drop target

--- targets/targets.mk
+++ targets/targets.mk
@@ -1,16 +1,28 @@
-GLUON_FEEDS='packages routing gluon'
+$(eval $(call GluonTarget,ath79,generic))
+$(eval $(call GluonTarget,ath79,nand))
+$(eval $(call GluonTarget,bcm27xx,bcm2708))
+$(eval $(call GluonTarget,bcm27xx,bcm2709))
+$(eval $(call GluonTarget,ipq40xx,generic))
+$(eval $(call GluonTarget,ipq806x,generic))
+$(eval $(call GluonTarget,lantiq,xrx200))
+$(eval $(call GluonTarget,lantiq,xway))
+$(eval $(call GluonTarget,mediatek,mt7622))
+$(eval $(call GluonTarget,mpc85xx,p1010))
+$(eval $(call GluonTarget,mpc85xx,p1020))
+$(eval $(call GluonTarget,ramips,mt7620))
+$(eval $(call GluonTarget,ramips,mt7621))
+$(eval $(call GluonTarget,ramips,mt76x8))
+$(eval $(call GluonTarget,ramips,rt305x))
+$(eval $(call GluonTarget,rockchip,armv8))
+$(eval $(call GluonTarget,sunxi,cortexa7))
+$(eval $(call GluonTarget,x86,generic))
+$(eval $(call GluonTarget,x86,geode))
+$(eval $(call GluonTarget,x86,legacy))
+$(eval $(call GluonTarget,x86,64))
 
-OPENWRT_REPO=https://github.com/openwrt/openwrt.git
-OPENWRT_BRANCH=openwrt-21.02
-OPENWRT_COMMIT=4aae7eb3c005b0a7548984fcf4c7e228244af163
 
-PACKAGES_PACKAGES_REPO=https://github.com/openwrt/packages.git
-PACKAGES_PACKAGES_BRANCH=openwrt-21.02
-PACKAGES_PACKAGES_COMMIT=a70f99a9124a8a8ef8351be7e830a5a22ffd190a
-
-PACKAGES_ROUTING_REPO=https://github.com/openwrt-routing/packages.git
-PACKAGES_ROUTING_BRANCH=openwrt-21.02
-PACKAGES_ROUTING_COMMIT=57533a25e84932a7e50b8483843c840f0924bc0c
-
-PACKAGES_GLUON_REPO=https://github.com/freifunk-gluon/packages.git
-PACKAGES_GLUON_COMMIT=8d53ff54e562ddb2ed8397781dd78edc76f6ff38
+ifneq ($(BROKEN),)
+$(eval $(call GluonTarget,bcm27xx,bcm2710)) # BROKEN: Untested
+$(eval $(call GluonTarget,bcm27xx,bcm2711)) # BROKEN: No WiFi support
+$(eval $(call GluonTarget,mvebu,cortexa9)) # BROKEN: No 11s support
+endif
