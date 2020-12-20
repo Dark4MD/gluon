069fa067ec09f93721b27271186f0ea858c294b0
lemoer <git@irrelefant.net>
2020-12-20 03:15:23 +0100

    treewide: rename "mesh-vpn" network interface to "vpn"
    This is a prepatory commit to create wireguard interfaces with the
    wireguard.sh netifd proto. As this name derives the interface name
    by the name of the section, interface names with dashes are not
    possible.

--- package/gluon-mesh-vpn-core/luasrc/usr/lib/lua/gluon/mesh-vpn.lua
+++ package/gluon-mesh-vpn-core/luasrc/usr/lib/lua/gluon/mesh-vpn.lua
@@ -1,47 +1,7 @@
-local uci = require('simple-uci').cursor()
-
-local util = require 'gluon.util'
-
 local M = {}
 
-function M.enabled()
-	return uci:get_bool('gluon', 'mesh_vpn', 'enabled')
-end
-
-function M.enable(val)
-	return uci:set('gluon', 'mesh_vpn', 'enabled', val)
-end
-
-function M.get_interface()
-	return 'mesh-vpn'
-end
-
-function M.get_provider(name)
-	return require('gluon.mesh-vpn.provider.' .. name)
-end
-
-function M.get_provider_names()
-	local out = {}
-
-	for _, v in ipairs(util.glob('/lib/gluon/mesh-vpn/provider/*')) do
-		table.insert(out, v:match('([^/]+)$'))
-	end
-
-	return out
-end
-
-function M.get_active_provider()
-	-- Active provider is the provider in use
-	-- by the currently active site / domain
-
-	for _, name in ipairs(M.get_provider_names()) do
-		local provider = M.get_provider(name)
-		if provider.active() then
-			return name, provider
-		end
-	end
-
-	return nil, nil
+function M.get_mesh_vpn_interface()
+  return 'vpn'
 end
 
 return M
